526b35d51c7297f07592ad70ecc9f06e52dd2c74
Adaamss <aademhamdi@gmail.com>
2026-04-24 12:02:20 +0100

    feat: add getPeopleStats service

--- src/service/personServices.ts
+++ src/service/personServices.ts
@@ -1,4 +1,4 @@
-import { Person } from "../types/person.type";
+import { Person, TYPE } from "../types/person.type";
 import { personList } from "../data/personData";
 
 export function filterById(id: number): Person[] {
@@ -11,4 +11,30 @@ export const addPerson = (body: Person): Person[] => {
   const { id, age, gender, type, name } = body;
   personList.push({ id, age, gender, type, name });
   return personList;
+};
+
+export const getPeopleStats = (): {
+  "Number of kids": number;
+  "Number of men": number;
+  "Number of women": number;
+} => {
+  let kids = 0;
+  let men = 0;
+  let women = 0;
+  for (const element of personList) {
+    if (element.type === TYPE.kid) {
+      kids++;
+    }
+    if (element.type === TYPE.men) {
+      men++;
+    }
+    if (element.type === TYPE.women) {
+      women++;
+    }
+  }
+  return {
+    "Number of kids": kids,
+    "Number of men": men,
+    "Number of women": women,
+  };
 };
