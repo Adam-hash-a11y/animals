830b4fb37a86173f6e6130618838bcdedb25c0c2
Adaamss <aademhamdi@gmail.com>
2026-04-26 11:20:56 +0100

    feat: add DELETE route for person by id

--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -4,9 +4,11 @@ import {
   addPersons,
   getStats,
   getPersons,
+  deletePersonById,
 } from "../controller/person.controller";
 
 export const personRouter = express.Router();
+personRouter.delete("/:id", deletePersonById);
 personRouter.get("/", getPersons);
 personRouter.get("/stats", getStats);
 personRouter.get("/:id", findPersonById);
